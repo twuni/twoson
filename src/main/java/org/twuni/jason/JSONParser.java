bdd7838802023e9244d76ce43f6bd3b7988e49da
Leo Accend <developer.aeonic@spamgourmet.com>
2013-07-23 02:14:59 -0700

    Added support for streaming atoi instead of using multiple buffers.

--- src/main/java/org/twuni/jason/JSONParser.java
+++ src/main/java/org/twuni/jason/JSONParser.java
@@ -45,213 +45,225 @@ public class JSONParser {
 		OBJECT_KEY
 	}
 
-	public void read() throws IOException {
-		byte [] buffer = new byte [64 * 1024];
-		Stack<Event> scope = new Stack<Event>();
-		for( int size = in.read( buffer, 0, buffer.length ); size > 0; size = in.read( buffer, 0, buffer.length ) ) {
-			for( int i = 0; i < buffer.length; i++ ) {
-				char c = (char) buffer[i];
-				switch( c ) {
-					case '{':
-						scope.push( Event.OBJECT );
-						listener.onBeginObject();
-						break;
-					case '}':
-						scope.pop();
-						listener.onEndObject();
-						break;
-					case '[':
-						scope.push( Event.ARRAY );
-						listener.onBeginArray();
-						break;
-					case ']':
-						scope.pop();
-						listener.onEndArray();
-						break;
-					case ',':
-						// Should only happen if our most recent event was an ARRAY or an OBJECT
-						break;
-					case ':':
-						// Should only happen if our most recent event was an OBJECT_KEY.
-						break;
-					case ' ':
-					case '\t':
-					case '\r':
-					case '\n':
-						// Ignore whitespace.
-						break;
-					case '"':
-						int j;
-						for( j = i + 1; j < buffer.length; j++ ) {
-							if( buffer[j] == '\\' ) {
-								j++;
-								continue;
-							}
-							if( buffer[j] == '"' ) {
-								break;
-							}
-						}
-						char [] string = new char [j - i - 1];
-						for( int a = 0; a < string.length; a++ ) {
-							string[a] = (char) buffer[i + 1 + a];
-						}
-						i = j;
-						switch( scope.peek() ) {
-							case OBJECT:
-								listener.onObjectKey( string );
-								burn( string );
-								scope.push( Event.OBJECT_KEY );
-								break;
-							case OBJECT_KEY:
-								listener.onString( string );
-								burn( string );
-								scope.pop();
-								break;
-							default:
-								listener.onString( string );
-								burn( string );
-								break;
-						}
-						break;
-					case 't':
-						// Assume this is true.
-						i += 3;
-						listener.onBoolean( true );
-						switch( scope.peek() ) {
-							case OBJECT_KEY:
-								scope.pop();
-								break;
-							default:
-								break;
-						}
-						break;
-					case 'f':
-						// Assume this is false.
-						i += 4;
-						listener.onBoolean( false );
-						switch( scope.peek() ) {
-							case OBJECT_KEY:
-								scope.pop();
-								break;
-							default:
-								break;
-						}
-					case 'n':
-						// Assume this is null.
-						i += 3;
-						listener.onNull();
-						switch( scope.peek() ) {
-							case OBJECT_KEY:
-								scope.pop();
-								break;
-							default:
-								break;
-						}
-					case '-':
-					case '0':
-					case '1':
-					case '2':
-					case '3':
-					case '4':
-					case '5':
-					case '6':
-					case '7':
-					case '8':
-					case '9':
-						int k;
-						boolean isFloat = false;
-						for( k = i + 1; k < buffer.length; k++ ) {
-							char z = (char) buffer[k];
-							if( z == '.' ) {
-								isFloat = true;
-								continue;
-							}
-							if( z < '0' || z > '9' ) {
-								break;
-							}
-						}
-						char [] number = new char [k - i];
-						for( int b = 0; b < number.length; b++ ) {
-							number[b] = (char) buffer[i + b];
-						}
-						if( isFloat ) {
-							float f = (float) atof( number );
-							burn( number );
-							listener.onFloat( f );
-						} else {
-							int g = atoi( number );
-							burn( number );
-							listener.onInteger( g );
-						}
-						i = k - 1;
-						switch( scope.peek() ) {
-							case OBJECT_KEY:
-								scope.pop();
-								break;
-							default:
-								break;
-						}
-						break;
-					default:
-						// Not sure what this is.
-						break;
-				}
-			}
+	private byte [] buffer = new byte [64 * 1024];
+	private int offset;
+	private int size;
+	private Stack<Event> scope = new Stack<Event>();
+
+	private char readNext() throws IOException {
+		if( offset >= size ) {
+			offset = 0;
+			size = in.read( buffer, 0, buffer.length );
 		}
+		if( isFinished() ) {
+			return '\0';
+		}
+		char c = (char) buffer[offset];
+		offset++;
+		return c;
+	}
+
+	private boolean isFinished() {
+		return size <= 0;
+	}
+
+	public void read() throws IOException {
+
+		size = in.read( buffer, 0, buffer.length );
+		for( char c = readNext(); c != '\0'; c = readNext() ) {
+
+			switch( c ) {
+
+				case '{':
+					scope.push( Event.OBJECT );
+					listener.onBeginObject();
+					break;
+
+				case '}':
+					scope.pop();
+					listener.onEndObject();
+					break;
+
+				case '[':
+					scope.push( Event.ARRAY );
+					listener.onBeginArray();
+					break;
+
+				case ']':
+					scope.pop();
+					listener.onEndArray();
+					break;
+
+				case ',':
+					// Should only happen if our most recent event was an ARRAY or an OBJECT
+					break;
+
+				case ':':
+					// Should only happen if our most recent event was an OBJECT_KEY.
+					break;
+
+				case ' ':
+				case '\t':
+				case '\r':
+				case '\n':
+					// Ignore whitespace.
+					break;
+
+				case '"':
+					int j;
+					// FIXME: Read-ahead only works properly if the right anchor exists within the
+					// buffer
+					for( j = offset; j < buffer.length; j++ ) {
+						if( buffer[j] == '\\' ) {
+							j++;
+							continue;
+						}
+						if( buffer[j] == '"' ) {
+							break;
+						}
+					}
+					char [] string = new char [j - offset];
+					for( int a = 0; a < string.length; a++ ) {
+						string[a] = (char) buffer[offset + a];
+					}
+					offset = j + 1;
+					switch( scope.peek() ) {
+						case OBJECT:
+							listener.onObjectKey( string );
+							burn( string );
+							scope.push( Event.OBJECT_KEY );
+							break;
+						case OBJECT_KEY:
+							listener.onString( string );
+							burn( string );
+							scope.pop();
+							break;
+						default:
+							listener.onString( string );
+							burn( string );
+							break;
+					}
+					break;
+
+				case 't':
+					readNext();// 'r'
+					readNext();// 'u'
+					readNext();// 'e'
+					listener.onBoolean( true );
+					switch( scope.peek() ) {
+						case OBJECT_KEY:
+							scope.pop();
+							break;
+						default:
+							break;
+					}
+					break;
+
+				case 'f':
+					readNext();// 'a'
+					readNext();// 'l'
+					readNext();// 's'
+					readNext();// 'e'
+					listener.onBoolean( false );
+					switch( scope.peek() ) {
+						case OBJECT_KEY:
+							scope.pop();
+							break;
+						default:
+							break;
+					}
+
+				case 'n':
+					readNext();// 'u'
+					readNext();// 'l'
+					readNext();// 'l'
+					listener.onNull();
+					switch( scope.peek() ) {
+						case OBJECT_KEY:
+							scope.pop();
+							break;
+						default:
+							break;
+					}
+
+				case '-':
+				case '0':
+				case '1':
+				case '2':
+				case '3':
+				case '4':
+				case '5':
+				case '6':
+				case '7':
+				case '8':
+				case '9':
+					double f = atof( c );
+					if( f - (int) f < 0.00001 ) {
+						listener.onInteger( (int) f );
+					} else {
+						listener.onFloat( (float) f );
+					}
+					switch( scope.peek() ) {
+						case OBJECT_KEY:
+							scope.pop();
+							break;
+						default:
+							break;
+					}
+					break;
+
+				default:
+					// Not sure what this is.
+					break;
+
+			}
+
+		}
+
 		burn( buffer );
+
 	}
 
 	private static boolean isDigit( char c ) {
 		return '0' <= c && c <= '9';
 	}
 
-	public static int atoi( char [] s ) {
-		int a = 0;
-		for( int i = 0; i < s.length; i++ ) {
-			a = a * 10 + ( s[i] - '0' );
-		}
-		return a;
-	}
-
-	public static double atof( char [] s ) {
-
+	private double atof( char firstDigit ) throws IOException {
 		double a = 0;
 		int e = 0;
-		int i;
-
-		for( i = 0; i < s.length; i++ ) {
-			if( !isDigit( s[i] ) ) {
-				break;
-			}
-			a = a * 10 + ( s[i] - '0' );
+		char c = firstDigit;
+		boolean negative = false;
+		if( c == '-' ) {
+			negative = true;
+			c = readNext();
 		}
-
-		if( s[i] == '.' ) {
-			i++;
-			while( i < s.length ) {
-				if( !isDigit( s[i] ) ) {
-					break;
-				}
-				a = a * 10 + ( s[i] - '0' );
+		while( isDigit( c ) ) {
+			a = a * 10 + ( c - '0' );
+			c = readNext();
+		}
+		if( c == '.' ) {
+			c = readNext();
+			while( isDigit( c ) ) {
+				a = a * 10 + ( c - '0' );
 				e--;
-				i++;
+				c = readNext();
 			}
 		}
-
-		if( i < s.length && ( s[i] == 'e' || s[i] == 'E' ) ) {
-
+		if( c == 'e' || c == 'E' ) {
 			int sign = 1;
 			int x = 0;
-			i++;
-			if( s[i] == '+' ) {
-				i++;
-			} else if( s[i] == '-' ) {
-				i++;
+			c = readNext();
+			if( c == '+' ) {
+				// Valid
+			} else if( c == '-' ) {
 				sign = -1;
+			} else {
+				throw new IOException( "Invalid number format" );
 			}
-			while( isDigit( s[i] ) ) {
-				x = x * 10 + ( s[i] - '0' );
-				i++;
+			c = readNext();
+			while( isDigit( c ) ) {
+				x = x * 10 + ( c - '0' );
+				c = readNext();
 			}
 			e += x * sign;
 		}
@@ -263,7 +275,8 @@ public class JSONParser {
 			a *= 0.1;
 			e++;
 		}
-		return a;
+		offset--;
+		return negative ? -a : a;
 	}
 
 	private static void burn( char [] buffer ) {
